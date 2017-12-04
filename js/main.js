44bf5078ae20a21fe41c8cfdbf169bfc3ad8fbca
Natalie Cyreus <nataliecyreus@gmail.com>
2017-12-04 12:14:01 -0500

    new admin button

--- js/main.js
+++ js/main.js
@@ -1,17 +1,130 @@
-var kittieName = '<h1>Kittie</h1>';
-$('#kittieName').append(kittieName);
-var kittieCount = 0;
-$('#kittieImage').click(function(e) {
-  kittieCount += 1;
-  $( "#clicks" ).empty();
-  $('#clicks').append(kittieCount);
-});
 
-var kittieName2 = '<h1>Kittie2</h1>';
-$('#kittieName2').append(kittieName2);
-var kittieCount2 = 0;
-$('#kittieImage2').click(function(e) {
-  kittieCount2 += 1;
-  $( "#clicks2" ).empty();
-  $('#clicks2').append(kittieCount2);
+$(function() {
+var catsArray = [];
+
+var Cats = function(){
+
+  function Cat(name , img){
+    this.name = name;
+    this.img = 'img/'+ img + '.jpg';
+    this.count = 0;
+  };
+
+    for (var i = 1; i <= 5; i++) {
+      var imageName = 'kitten' + i;
+      object = new Cat (imageName,imageName);
+      catsArray.push(object);
+    }
+    return catsArray;
+  }
+
+
+var model = {
+    currentCat : null,
+    cats : Cats()
+
+};
+
+var octopus = {
+    init: function() {
+      model.currentCat = model.cats[0];
+      buttonView.init();
+      catView.init();
+      adminView.init();
+    },
+
+    getCurrentCat: function() {
+      return model.currentCat;
+    },
+
+    //returns the catsArray
+    getCats: function() {
+      return model.cats;
+    },
+
+    // set the currently-selected cat to the object passed in
+    setCurrentCat: function(cat){
+      model.currentCat = cat;
+    },
+
+    // increments the counter for the currently-selected cat
+    incrementCounter : function() {
+      model.currentCat.count++;
+      catView.render();
+    }
+};
+
+var catView = {
+  init: function() {
+    // store pointers to our DOM elements for easy access later
+    this.catElem = document.getElementById('cat');
+    this.catNameElem = document.getElementById('kittenName');
+    this.catImageElem = document.getElementById('catImg');
+    this.countElem = document.getElementById('clicks');
+    // on click, increment the current cat's counter
+    this.catImageElem.addEventListener('click', function() {
+      octopus.incrementCounter();
+    });
+
+    // render this view (update the DOM elements with the right values)
+    this.render();
+  },
+
+  render: function() {
+    var currentCat = octopus.getCurrentCat();
+    this.countElem.textContent = currentCat.count;
+    this.catNameElem.textContent = currentCat.name;
+    this.catImageElem.src = currentCat.img;
+  }
+
+};
+
+
+var buttonView = {
+
+    init : function() {
+      this.buttonElem = document.getElementById('catButtons');
+      this.render();
+    },
+    render: function() {
+      var cat, elem, i;
+      var cats = octopus.getCats();
+      this.buttonElem.innerHTML = '';
+
+
+      for (i = 0; i < cats.length; i++){
+
+        cat = cats[i];
+        elem = document.createElement('button');
+        elem.textContent = cat.name;
+
+        elem.addEventListener('click', (function(catCopy) {
+          return function() {
+            octopus.setCurrentCat(catCopy);
+            catView.render();
+            adminView.init();
+          };
+        })(cat));
+
+        this.buttonElem.appendChild(elem);
+      }
+    }
+  };
+
+  var adminView = {
+    init: function() {
+      adminView.render();
+      },
+    render: function() {
+      var currentCat = octopus.getCurrentCat();
+      this.catName = document.getElementById('catName');
+      this.catUrl = document.getElementById('catUrl');
+      this.catClicks = document.getElementById('catClicks');
+      this.catName.value = currentCat.name;
+      this.catUrl.value = currentCat.img;
+      this.catClicks.value = currentCat.count;
+    }
+  };
+
+octopus.init();
 });
